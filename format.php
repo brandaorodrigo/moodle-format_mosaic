2ae0b4519429a596037a3b1e9413dd5543ae831d
rodrigo brandão <rodrigo.brandao@spotmetrics.com>
2023-03-02 12:29:03 -0300

    fix

--- format.php
+++ format.php
@@ -13,65 +13,64 @@ function url($name, $id)
     return $CFG->wwwroot . '/course/format/mosaic/base64.php?name=' . $name . '&id=' . $id;
 }
 
-echo '<style>
+?>
+<style>
+    body:not(.editing) .available_percent .completed,
+    body:not(.editing) .course-content #section-0,
+    body:not(.editing) .button_enter_section {
+        background-color: <?php echo $course->color_main ?> !important;
+    }
 
-body:not(.editing) .available_percent .completed,
-body:not(.editing) .course-content #section-0,
-body:not(.editing) .button_enter_section {
-    background-color: ' . $course->color_main . ' !important;
-}
+    body:not(.editing) .course-content #section-0 {
+        background-image: url(<?php echo url(' base64_section0', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-0 {
-    background-color: ' . $course->color_main . ' !important;
-    background-image: url(' . url('base64_section0', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-1 {
+        background-image: url(<?php echo url(' base64_section1', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-1 {
-    background-image: url(' . url('base64_section1', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-1:hover {
+        background-image: url(<?php echo url(' base64_section1_hover', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-1:hover {
-    background-image: url(' . url('base64_section1_hover', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-1.section-open {
+        background-image: url(<?php echo url(' base64_section1_full', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-1.section-open {
-    background-image: url(' . url('base64_section1_full', $course->id) . ') !important;
-}
+    body:not(.editing) .course-content #section-2 {
+        background-image: url(<?php echo url(' base64_section2', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-2 {
-    background-image: url(' . url('base64_section2', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-2:hover {
+        background-image: url(<?php echo url(' base64_section2_hover', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-2:hover {
-    background-image: url(' . url('base64_section2_hover', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-2.section-open {
+        background-image: url(<?php echo url(' base64_section2_full', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-2.section-open {
-    background-image: url(' . url('base64_section2_full', $course->id) . ') !important;
-}
+    body:not(.editing) .course-content #section-3 {
+        background-image: url(<?php echo url(' base64_section3', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-3 {
-    background-image: url(' . url('base64_section3', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-3:hover {
+        background-image: url(<?php echo url(' base64_section3_hover', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-3:hover {
-    background-image: url(' . url('base64_section3_hover', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-3.section-open {
+        background-image: url(<?php echo url(' base64_section3_full', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-3.section-open {
-    background-image: url(' . url('base64_section3_full', $course->id) . ') !important;
-}
+    body:not(.editing) .course-content #section-4 {
+        background-image: url(<?php echo url(' base64_section4', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-4 {
-    background-image: url(' . url('base64_section4', $course->id) . ');
-}
+    body:not(.editing) .course-content #section-4:hover {
+        background-image: url(<?php echo url(' base64_section4_hover', $course->id) ?>);
+    }
 
-body:not(.editing) .course-content #section-4:hover {
-    background-image: url(' . url('base64_section4_hover', $course->id) . ');
-}
-
-body:not(.editing) .course-content #section-4.section-open {
-    background-image: url(' . url('base64_section4_full', $course->id) . ') !important;
-}
-
-</style>';
+    body:not(.editing) .course-content #section-4.section-open {
+        background-image: url(<?php echo url(' base64_section4_full', $course->id) ?>);
+    }
+</style>
+<?php
