575bb96ecbd2db6977e802e8205d4cc4ae44c0aa
rodrigo brandão <rodrigo.brandao@spotmetrics.com>
2023-03-31 16:52:23 -0300

    fix

--- version.php
+++ version.php
@@ -2,14 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023033002;
+$plugin->version   = 2023033101;
 $plugin->requires  = 2022111800;
 $plugin->component = 'format_mosaic';
-
-/*
-
-- Textos para os helps de tamanho das imagens.
-- Animacao esconder texto enquanto abre.
-- Uma cor para cada curso.
-
-*/
